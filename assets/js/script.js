820b98ace3df3bba474a8071e351ffbd4014597e
stroemlind <elin.stromlind@gmail.com>
2021-10-28 09:49:44 +0000

    Add js form validation and eventListener to button

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+/**The username registration form
+ * called when the user registrer their username 
+ * and starts the game
+ */
+
+let form = document.getElementsByClassName('username-form');
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    let username = [];
+
+    for (let name in username) {
+        name = document.getElementsByClassName('username').value;
+        username.push(name);
+    }
+
+    form.submit();
+
+    console.log(username);
+}
+
+//When submit button clicked, take user to the game 
+// When the user click the button to submit username and start the quiz
+let buttons = document.getElementsByTagName('button');
+
+for (let button of buttons) {
+    button.addEventListener('click', function () {
+        if (this.getAttribute('data-type') === 'submit') {
+            goQuiz();
+        }
+    });
+}
+
+// When user press "Enter"-key to submit username and start the quiz
+document.getElementsByClassName('username').addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        GoQuiz();
+    }
+});
