76b511e7b79cdf9361ea548b5f0e93d79dfcb81b
stroemlind <elin.stromlind@gmail.com>
2021-10-28 12:50:06 +0000

    Add questions to quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,3 +44,96 @@ document.getElementsByClassName('username').addEventListener('keydown', function
  * the question, score keeping 
  */
 
+//The question form the quiz
+let quizQuestion = [
+    {
+        question: 'Who is the hit-making producer behind Britney Spears song “... Baby One More Time”?',
+        answers: {
+            a: 'Max Martin',
+            b: 'Rick Rubin',
+            c: 'Mark Ronson'
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: 'Which of the following songs is not a song released by the group EXO?',
+        answers: {
+            a: 'Growl',
+            b: 'Wolf',
+            c: 'Howl'
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: 'What year did ABBA release their song “Dancing Queen”?',
+        answers: {
+            a: '1974',
+            b: '1976',
+            c: '1979'
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: 'On which album did Prince release the song “Let’s Go Crazy”?',
+        answers: {
+            a: 'Parade',
+            b: 'Purple Rain',
+            c: 'Around the World in a Day'
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: 'What’s the name of Harry Styles debut song?',
+        answers: {
+            a: 'Sign of the Times',
+            b: 'Sweet Creature',
+            c: 'Lights Up'
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: 'Which artist wants us all to live a “Lush life”?',
+        answers: {
+            a: 'Dua Lipa',
+            b: 'Taylor Swift',
+            c: 'Zara Larsson'
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: 'With a music video that got as much attention as their hit song “Take On Me”, in 1985, A-HA gained international recognition. Do you know which country they are from?',
+        answers: {
+            a: 'Sweden',
+            b: 'Norway',
+            c: 'Denmark'
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: 'Coldplay released a song together with BTS. What\'s the name of that song?',
+        answers: {
+            a: 'Univers',
+            b: 'Army',
+            c: 'Butter'
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: 'Before being a solo artist, Camila Cabello was in a group, which group?',
+        answers: {
+            a: 'Little Mix',
+            b: 'Fifth Harmony',
+            c: 'Blackpink'
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: 'Which year did the group Backstreet Boys form?',
+        answers: {
+            a: '1995',
+            b: '1993',
+            c: '1996'
+        },
+        correctAnswer: 'b'
+    }
+]
