3ea2b73c05c7715e144603c5bad5b3666ac2fc7a
stroemlind <elin.stromlind@gmail.com>
2021-10-29 13:28:25 +0000

    Add structure to quizgame in javaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,177 +1,123 @@
-/**The username registration form
- * called when the user registrer their username 
- * and starts the game
- */
-
-let form = document.getElementsByClassName('username-form');
-form.addEventListener('submit', handleSubmit);
-
-function handleSubmit(event) {
-    event.preventDefault();
-
-    let username = [];
-
-    for (let name in username) {
-        name = document.getElementsByClassName('username').value;
-        username.push(name);
-    }
-
-    form.submit();
-
-    console.log(username);
-}
-
-//When submit button clicked, take user to the game 
-// When the user click the button to submit username and start the quiz
-let buttons = document.getElementsByTagName('button');
-
-for (let button of buttons) {
-    button.addEventListener('click', function () {
-        if (this.getAttribute('data-type') === 'submit') {
-            goQuiz();
-        }
-    });
-}
-
-// When user press "Enter"-key to submit username and start the quiz
-document.getElementsByClassName('username').addEventListener('keydown', function(event) {
-    if (event.key === 'Enter') {
-        GoQuiz();
-    }
-});
-
 /**The structure for the quiz
  * the question, score keeping 
  */
 
+let startButton = document.getElementById('start-button');
+let quizDiv = document.getElementById('quiz');
+let questionDiv = document.getElementById('question');
+let answerButtons = document.getElementById('answer');
+let randomQuestion, currentQuestionIndex
+
+startButton.addEventListener('click', goGame);
+
+
+//The quiz game function
 function goGame() {
-
-    //Building the quize function
-    function quizStructure() {
-        let output = []; 
-
-        quizQuestion.forEach(
-            (currentQuestion, questionNumber) => {
-                let answers = [];
-    
-                for(letter in currentQuestion.answers) {
-                    answers.push(
-                        `<label>
-                            <input type="radio" name="question${questionNumber}" value="${letter}">
-                            ${letter} :
-                            ${currentQuestion.answers[letter]}
-                        </label>`
-                    );
-                }
-
-                output.push(
-                    `<div class="question">${currentQuestion.question}</div>
-                    <div class="answers">${answers.join('')}</div>`
-                );
-            }
-        );
-
-        quizDiv.innerHTML = output.join('');
-    } 
-
-
-    let quizDiv = document.getElementById('quiz');
-    let resultDiv = document.getElementById('result');
-    let quizSubmit = document.getElementById('submitQuiz');
-
-    //The question for the quiz
-    let quizQuestion = [
-        {
-            question: 'Who is the hit-making producer behind Britney Spears song “... Baby One More Time”?',
-            answers: {
-                a: 'Max Martin',
-                b: 'Rick Rubin',
-                c: 'Mark Ronson'
-            },
-            correctAnswer: 'a'
-        },
-        {
-            question: 'Which of the following songs is not a song released by the group EXO?',
-            answers: {
-                a: 'Growl',
-                b: 'Wolf',
-                c: 'Howl'
-            },
-            correctAnswer: 'c'
-        },
-        {
-            question: 'What year did ABBA release their song “Dancing Queen”?',
-            answers: {
-                a: '1974',
-                b: '1976',
-                c: '1979'
-            },
-            correctAnswer: 'b'
-        },
-        {
-            question: 'On which album did Prince release the song “Let’s Go Crazy”?',
-            answers: {
-                a: 'Parade',
-                b: 'Purple Rain',
-                c: 'Around the World in a Day'
-            },
-            correctAnswer: 'b'
-        },
-        {
-            question: 'What’s the name of Harry Styles debut song?',
-            answers: {
-                a: 'Sign of the Times',
-                b: 'Sweet Creature',
-                c: 'Lights Up'
-            },
-            correctAnswer: 'a'
-        },
-        {
-            question: 'Which artist wants us all to live a “Lush life”?',
-            answers: {
-                a: 'Dua Lipa',
-                b: 'Taylor Swift',
-                c: 'Zara Larsson'
-            },
-            correctAnswer: 'c'
-        },
-        {
-            question: 'With a music video that got as much attention as their hit song “Take On Me”, in 1985, A-HA gained international recognition. Do you know which country they are from?',
-            answers: {
-                a: 'Sweden',
-                b: 'Norway',
-                c: 'Denmark'
-            },
-            correctAnswer: 'b'
-        },
-        {
-            question: 'Coldplay released a song together with BTS. What\'s the name of that song?',
-            answers: {
-                a: 'Univers',
-                b: 'Army',
-                c: 'Butter'
-            },
-            correctAnswer: 'a'
-        },
-        {
-            question: 'Before being a solo artist, Camila Cabello was in a group, which group?',
-            answers: {
-                a: 'Little Mix',
-                b: 'Fifth Harmony',
-                c: 'Blackpink'
-            },
-            correctAnswer: 'b'
-        },
-        {
-            question: 'Which year did the group Backstreet Boys form?',
-            answers: {
-                a: '1995',
-                b: '1993',
-                c: '1996'
-            },
-            correctAnswer: 'b'
-        }
-    ];
-
-    quizStructure();
+    console.log('started')
+    randomQuestion = question.sort(() => Math.random() - .5);
+    currentQuestionIndex = 0;
+    setNextQuestion();
 }
+
+//Setting the next question
+function setNextQuestion() {
+    viewQuestion(randomQuestion[currentQuestionIndex]);
+}
+
+//Showing the question
+function viewQuestion(question) {
+    questionDiv.innerText = question.question
+    question.answers.forEach(answer => {
+        let button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+    });
+}
+
+ //The question for the quiz
+ let quizQuestion = [
+     {
+         question: 'Who is the hit-making producer behind Britney Spears song “... Baby One More Time”?',
+         answers: [
+             {text: 'Max Martin', correct: true},
+             {text: 'Rick Rubin', correct: false},
+             {text: 'Mark Ronson', correct: false}
+         ],
+     },
+     {
+         question: 'Which of the following songs is not a song released by the group EXO?',
+         answers: [
+             {text: 'Growl', correct: false},
+             {text: 'Wolf', correct: false},
+             {text: 'Howl', correct: true}
+         ],
+     },
+     {
+         question: 'What year did ABBA release their song “Dancing Queen”?',
+         answers: [
+             {text: '1974', correct: false},
+             {text: '1976', correct: true},
+             {text: '1979', correct: false}
+         ],
+     },
+     {
+         question: 'On which album did Prince release the song “Let’s Go Crazy”?',
+         answers: [
+             {text: 'Parade', correct: false},
+             {text: 'Purple Rain', correct: true},
+             {text: 'Around the World in a Day', correct: false}
+         ],
+     },
+     {
+         question: 'What’s the name of Harry Styles debut song?',
+         answers: [
+             {text: 'Sign of the Times', correct: true},
+             {text: 'Sweet Creature', correct: false},
+             {text: 'Lights Up', correct: false}
+         ],
+     },
+     {
+         question: 'Which artist wants us all to live a “Lush life”?',
+         answers: [
+             {text: 'Dua Lipa', correct: false},
+             {text: 'Taylor Swift', correct: false},
+             {text: 'Zara Larsson', correct: true}
+         ],
+     },
+     {
+         question: 'With a music video that got as much attention as their hit song “Take On Me”, in 1985, A-HA gained international recognition. Do you know which country they are from?',
+         answers: [
+             {text: 'Sweden', correct: false},
+             {text: 'Norway', correct: true},
+             {text: 'Denmark', correct: false}
+         ],
+     },
+     {
+         question: 'Coldplay released a song together with BTS. What\'s the name of that song?',
+         answers: [
+             {text: 'Univers', correct: true},
+             {text: 'Army', correct: false},
+             {text: 'Butter', correct: false}
+         ],
+     },
+     {
+         question: 'Before being a solo artist, Camila Cabello was in a group, which group?',
+         answers: [
+             {text: 'Little Mix', correct: false},
+             {text: 'Fifth Harmony', correct: true},
+             {text: 'Blackpink', correct: false}
+         ],
+     },
+     {
+         question: 'Which year did the group Backstreet Boys form?',
+         answers: [
+             {text: '1995', correct: false},
+             {text: '1993', correct: true},
+             {text: '1996', correct: false}
+         ],
+     }
+ ];
