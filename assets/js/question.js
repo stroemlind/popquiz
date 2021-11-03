91c64f9a6f34974d5bd1faf04e440edd89f9d6f4
stroemlind <elin.stromlind@gmail.com>
2021-11-03 17:50:33 +0000

    Fix bug with pickQuestion function

--- assets/js/question.js
+++ assets/js/question.js
@@ -1,133 +1,260 @@
  /* jshint esversion: 8 */
- 
+
  //The question for the quiz
- const quizQuestion = [
-    {
-        question: 'Who is the hit-making producer behind Britney Spears song “... Baby One More Time”?',
-        answers: [
-            {text: 'Max Martin', correct: true},
-            {text: 'Rick Rubin', correct: false},
-            {text: 'Mark Ronson', correct: false}
-        ],
-    },
-    {
-        question: 'Which of the following songs is not a song released by the group EXO?',
-        answers: [
-            {text: 'Growl', correct: false},
-            {text: 'Wolf', correct: false},
-            {text: 'Howl', correct: true}
-        ],
-    },
-    {
-        question: 'What year did ABBA release their song “Dancing Queen”?',
-        answers: [
-            {text: '1974', correct: false},
-            {text: '1976', correct: true},
-            {text: '1979', correct: false}
-        ],
-    },
-    {
-        question: 'On which album did Prince release the song “Let’s Go Crazy”?',
-        answers: [
-            {text: 'Parade', correct: false},
-            {text: 'Purple Rain', correct: true},
-            {text: 'Around the World in a Day', correct: false}
-        ],
-    },
-    {
-        question: 'What’s the name of Harry Styles debut song?',
-        answers: [
-            {text: 'Sign of the Times', correct: true},
-            {text: 'Sweet Creature', correct: false},
-            {text: 'Lights Up', correct: false}
-        ],
-    },
-    {
-        question: 'Which artist wants us all to live a “Lush life”?',
-        answers: [
-            {text: 'Dua Lipa', correct: false},
-            {text: 'Taylor Swift', correct: false},
-            {text: 'Zara Larsson', correct: true}
-        ],
-    },
-    {
-        question: 'With a music video that got as much attention as their hit song “Take On Me”, in 1985, A-HA gained international recognition. Do you know which country they are from?',
-        answers: [
-            {text: 'Sweden', correct: false},
-            {text: 'Norway', correct: true},
-            {text: 'Denmark', correct: false}
-        ],
-    },
-    {
-        question: 'Coldplay released a song together with BTS. What\'s the name of that song?',
-        answers: [
-            {text: 'Univers', correct: true},
-            {text: 'Army', correct: false},
-            {text: 'Butter', correct: false}
-        ],
-    },
-    {
-        question: 'Before being a solo artist, Camila Cabello was in a group, which group?',
-        answers: [
-            {text: 'Little Mix', correct: false},
-            {text: 'Fifth Harmony', correct: true},
-            {text: 'Blackpink', correct: false}
-        ],
-    },
-    {
-        question: 'Which year did the group Backstreet Boys form?',
-        answers: [
-            {text: '1995', correct: false},
-            {text: '1993', correct: true},
-            {text: '1996', correct: false}
-        ],
-    },
-    {
-        question: 'Which of the following songs is a song by NSYNC?',
-        answers: [
-            {text: 'Keep On Movin’ ', correct: false},
-            {text: 'My Love', correct: false},
-            {text: 'It’/s Gonna Be Me', correct: true}
-        ],
-    },
-    {
-        question: 'What year was Rihannas third studio album “Good Girl Gone Bad” relesed?',
-        answers: [
-            {text: '2006', correct: false},
-            {text: '2007', correct: true},
-            {text: '2008', correct: false}
-        ],
-    },
-    {
-        question: 'What is the name of Michael Jacksons album where you can find the songs like "Beat It" and "Billie Jean"?',
-        answers: [
-            {text: 'Thriller', correct: true},
-            {text: 'Bad', correct: false},
-            {text: 'Dangerous', correct: false}
-        ],
-    },
-    {
-        question: 'How many Grammy Awards did Bruno Mars receive for his album "24K Gold"?',
-        answers: [
-            {text: '7', correct: true},
-            {text: '9', correct: false},
-            {text: '12', correct: false}
-        ],
-    },
-    {
-        question: 'Which of the following songs did Dua Lipa win The British Music Awards “Song of the year” in 2019?',
-        answers: [
-            {text: 'New Rules', correct: false},
-            {text: "Don't Start Now", correct: false},
-            {text: 'One Kiss', correct: true}
-        ],
-    },
-    {
-        question: 'The song "Old Town Road" spent 19 weeks on the US Billboard Hot 100 chart, becoming the longest-running number-one song since the chart debuted in 1958. Who is the artist performing the song? ',
-        answers: [
-            {text: 'Beyonce', correct: false},
-            {text: 'Taylor Swift', correct: false},
-            {text: 'Lil Nas X', correct: true}
-        ],
-    }
-];
+ const quizQuestion = [{
+         question: 'Who is the hit-making producer behind Britney Spears song “... Baby One More Time”?',
+         answers: [{
+                 text: 'Max Martin',
+                 correct: true
+             },
+             {
+                 text: 'Rick Rubin',
+                 correct: false
+             },
+             {
+                 text: 'Mark Ronson',
+                 correct: false
+             }
+         ],
+     },
+     {
+         question: 'Which of the following songs is not a song released by the group EXO?',
+         answers: [{
+                 text: 'Growl',
+                 correct: false
+             },
+             {
+                 text: 'Wolf',
+                 correct: false
+             },
+             {
+                 text: 'Howl',
+                 correct: true
+             }
+         ],
+     },
+     {
+         question: 'What year did ABBA release their song “Dancing Queen”?',
+         answers: [{
+                 text: '1974',
+                 correct: false
+             },
+             {
+                 text: '1976',
+                 correct: true
+             },
+             {
+                 text: '1979',
+                 correct: false
+             }
+         ],
+     },
+     {
+         question: 'On which album did Prince release the song “Let’s Go Crazy”?',
+         answers: [{
+                 text: 'Parade',
+                 correct: false
+             },
+             {
+                 text: 'Purple Rain',
+                 correct: true
+             },
+             {
+                 text: 'Around the World in a Day',
+                 correct: false
+             }
+         ],
+     },
+     {
+         question: 'What’s the name of Harry Styles debut song?',
+         answers: [{
+                 text: 'Sign of the Times',
+                 correct: true
+             },
+             {
+                 text: 'Sweet Creature',
+                 correct: false
+             },
+             {
+                 text: 'Lights Up',
+                 correct: false
+             }
+         ],
+     },
+     {
+         question: 'Which artist wants us all to live a “Lush life”?',
+         answers: [{
+                 text: 'Dua Lipa',
+                 correct: false
+             },
+             {
+                 text: 'Taylor Swift',
+                 correct: false
+             },
+             {
+                 text: 'Zara Larsson',
+                 correct: true
+             }
+         ],
+     },
+     {
+         question: 'With a music video that got as much attention as their hit song “Take On Me”, in 1985, A-HA gained international recognition. Do you know which country they are from?',
+         answers: [{
+                 text: 'Sweden',
+                 correct: false
+             },
+             {
+                 text: 'Norway',
+                 correct: true
+             },
+             {
+                 text: 'Denmark',
+                 correct: false
+             }
+         ],
+     },
+     {
+         question: 'Coldplay released a song together with BTS. What\'s the name of that song?',
+         answers: [{
+                 text: 'Univers',
+                 correct: true
+             },
+             {
+                 text: 'Army',
+                 correct: false
+             },
+             {
+                 text: 'Butter',
+                 correct: false
+             }
+         ],
+     },
+     {
+         question: 'Before being a solo artist, Camila Cabello was in a group, which group?',
+         answers: [{
+                 text: 'Little Mix',
+                 correct: false
+             },
+             {
+                 text: 'Fifth Harmony',
+                 correct: true
+             },
+             {
+                 text: 'Blackpink',
+                 correct: false
+             }
+         ],
+     },
+     {
+         question: 'Which year did the group Backstreet Boys form?',
+         answers: [{
+                 text: '1995',
+                 correct: false
+             },
+             {
+                 text: '1993',
+                 correct: true
+             },
+             {
+                 text: '1996',
+                 correct: false
+             }
+         ],
+     },
+     {
+         question: 'Which of the following songs is a song by NSYNC?',
+         answers: [{
+                 text: "Keep On Movin",
+                 correct: false
+             },
+             {
+                 text: 'My Love',
+                 correct: false
+             },
+             {
+                 text: "It’s Gonna Be Me",
+                 correct: true
+             }
+         ],
+     },
+     {
+         question: 'What year was Rihannas third studio album “Good Girl Gone Bad” relesed?',
+         answers: [{
+                 text: '2006',
+                 correct: false
+             },
+             {
+                 text: '2007',
+                 correct: true
+             },
+             {
+                 text: '2008',
+                 correct: false
+             }
+         ],
+     },
+     {
+         question: 'What is the name of Michael Jacksons album where you can find the songs like "Beat It" and "Billie Jean"?',
+         answers: [{
+                 text: 'Thriller',
+                 correct: true
+             },
+             {
+                 text: 'Bad',
+                 correct: false
+             },
+             {
+                 text: 'Dangerous',
+                 correct: false
+             }
+         ],
+     },
+     {
+         question: 'How many Grammy Awards did Bruno Mars receive for his album "24K Gold"?',
+         answers: [{
+                 text: '7',
+                 correct: true
+             },
+             {
+                 text: '9',
+                 correct: false
+             },
+             {
+                 text: '12',
+                 correct: false
+             }
+         ],
+     },
+     {
+         question: 'Which of the following songs did Dua Lipa win The British Music Awards “Song of the year” in 2019?',
+         answers: [{
+                 text: 'New Rules',
+                 correct: false
+             },
+             {
+                 text: "Don't Start Now",
+                 correct: false
+             },
+             {
+                 text: 'One Kiss',
+                 correct: true
+             }
+         ],
+     },
+     {
+         question: 'The song "Old Town Road" spent 19 weeks on the US Billboard Hot 100 chart, becoming the longest-running number-one song since the chart debuted in 1958. Who is the artist performing the song?',
+         answers: [{
+                 text: 'Beyonce',
+                 correct: false
+             },
+             {
+                 text: 'Taylor Swift',
+                 correct: false
+             },
+             {
+                 text: 'Lil Nas X',
+                 correct: true
+             }
+         ],
+     }
+ ];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,162 +1,149 @@
 /* jshint esversion: 8 */
 
-/**The username registration form
- * called when the user registrer their username 
- * and starts the game
- */
+if (location.pathname === '/' || location.pathname == '/index.html') {
+    /**The username registration form
+     * called when the user registrer their username 
+     * and starts the game
+     */
 
-let form = document.querySelector('.username-form');
-let usernameList = JSON.parse(localStorage.getItem('usernameList')) || [];
+    //Adds username to local storage 
+    let form = document.querySelector('.username-form');
+    //let usernameList = JSON.parse(localStorage.getItem('usernameList')) || [];
 
-function addUser(event) {
-    event.preventDefault();
+    function addUser(event) {
+        event.preventDefault();
 
-    let text = (this.querySelector('[name=username]')).value;
-    let user = {
-        text
-    };
-    console.log(user);
-    usernameList.push(user);
-    localStorage.setItem('usernameList', JSON.stringify(usernameList));
-    this.reset();
-}
+        //let text = (this.querySelector('[name=username]')).value;
 
+        localStorage.setItem('username', this.querySelector('[name=username]').value);
+        //this.reset();
+        window.location = '/game.html'
+    }
 
+    form.addEventListener('submit', addUser);
 
-form.addEventListener('submit', addUser);
-
-/**The structure for the quiz with
- * the question, 
- * game functions, 
- * score keeping and
- * showing final result
- */
-
-//Getting elements from the game.html file
-let startButton = document.getElementById('start-button');
-let nextButton = document.getElementById('submitQuiz');
-let submitButton = document.getElementById('done');
-let controlButtons = document.getElementById('control-btns');
-let quizDiv = document.getElementById('quiz');
-let questionDiv = document.getElementById('question');
-let answerButtons = document.getElementById('answer');
-let trackScore = document.getElementById('score-tracking');
-let resultDiv = document.getElementById('result');
-let resetButton = document.getElementById('reset');
-let randomNumber;
-let usedQuestions = [];
-
-answerButtons.classList.add('hide');
-
-/*if (location.href === "game.html") {
-    all the game functionality;
 } else {
-    all the index functionality;
-}*/
 
-let quizCopy = quizQuestion.slice();
+    /**The structure for the quiz with
+     * the question, 
+     * game functions, 
+     * score keeping and
+     * showing final result
+     */
+
+    //Getting elements from the game.html file
+    let startButton = document.getElementById('start-button');
+    let nextButton = document.getElementById('submitQuiz');
+    let submitButton = document.getElementById('done');
+    let controlButtons = document.getElementById('control-btns');
+    let quizDiv = document.getElementById('quiz');
+    let questionDiv = document.getElementById('question');
+    let answerButtons = document.getElementById('answer');
+    let trackScore = document.getElementById('score-tracking');
+    let resultDiv = document.getElementById('result');
+    let resetButton = document.getElementById('reset');
+    let randomNumber;
+    let usedQuestions = [];
+
+    let quizCopy = quizQuestion.slice();
 
 
-//Game buttons calling event
-startButton.addEventListener('click', goGame);
-nextButton.addEventListener('click', checkAnswer);
-submitButton.addEventListener('click', checkAnswer);
-resetButton.addEventListener('click', reset);
 
-//The quiz game functions
+    //Game buttons calling event
+    startButton.addEventListener('click', goGame);
+    nextButton.addEventListener('click', checkAnswer);
+    submitButton.addEventListener('click', checkAnswer);
+    resetButton.addEventListener('click', reset);
 
-//Picks a random question everytime someone starts the game
-function pickQuestion() {
-    
-    //Unchecks radio input when going to the next question
-    let answer = document.getElementsByName('answer');
-    for(let i = 0; i < answer.length; i++) {
-        if(answer[i].checked) {
-            answer[i].checked = false;
+    //The quiz game functions
+
+    //Picks a random question everytime someone starts the game
+    function pickQuestion() {
+
+        //Unchecks radio input when going to the next question
+        let answer = document.getElementsByName('answer');
+        for (let i = 0; i < answer.length; i++) {
+            if (answer[i].checked) {
+                answer[i].checked = false;
+            }
         }
+
+        if (quizQuestion.length === 7) {
+            submitButton.classList.remove('hide');
+            nextButton.classList.add('hide');
+        } else if (quizQuestion.length === 0 || quizQuestion.length <= 6) {
+            endGame();
+        }
+
+        //Show a random question everytime the quiz starts
+        if (quizQuestion.length !== 0) {
+            randomNumber = Math.floor(Math.random() * quizQuestion.length);
+
+            document.getElementById('question').innerHTML = quizQuestion[randomNumber].question;
+            document.getElementById('a1').innerText = quizQuestion[randomNumber].answers[0].text;
+            document.getElementById('a2').innerText = quizQuestion[randomNumber].answers[1].text;
+            document.getElementById('a3').innerText = quizQuestion[randomNumber].answers[2].text;
+
+            usedQuestions.push(quizQuestion[randomNumber]);
+            quizQuestion.splice(randomNumber, 1);
+        }
+
     }
 
-    if(quizQuestion.length == 1) {
-        submitButton.classList.remove('hide');
-        nextButton.classList.add('hide');
-    } else if(quizQuestion.length == 0) {
-        endGame();
-    }
-
-    //Show a random question everytime the quiz starts
-    if(quizQuestion.length !== 0) {
-        randomNumber = Math.floor(Math.random() * 10);
-
-        document.getElementById('question').innerHTML = quizQuestion[randomNumber].question;
-        document.getElementById('a1').innerText = quizQuestion[randomNumber].answers[0].text;
-        document.getElementById('a2').innerText = quizQuestion[randomNumber].answers[1].text;
-        document.getElementById('a3').innerText = quizQuestion[randomNumber].answers[2].text;
-    
-        console.log('First');
-
-        usedQuestions.push(quizQuestion[randomNumber]);
-        quizQuestion.splice(randomNumber, 1);
-    }
-    
-}
-
-//Checks if the answer is correct or incorrect
-function checkAnswer() {
-    let answer = document.getElementsByName('answer');
-    for(let i = 0; i < answer.length; i++) {
-        if(answer[i].checked) {
-            if(quizCopy[randomNumber].answers[i].correct) {
+    //Checks if the answer is correct or incorrect
+    function checkAnswer() {
+        let answer = document.getElementsByName('answer');
+        for (let i = 0; i < answer.length; i++) {
+            if (answer[i].checked) {
+                if (quizCopy[randomNumber].answers[i].correct) {
+                    incrementScore();
+                } else {
+                    wrongScore();
+                }
                 quizCopy.splice(randomNumber, 1);
-                incrementScore();
-            } else {
-                quizCopy.splice(randomNumber, 1);
-                wrongScore();
             }
         }
     }
-}
 
-//Score tracking 
-function incrementScore() {
-    let oldScore = parseInt(document.getElementById('correct').innerText);
-    document.getElementById('correct').innerText = ++oldScore;
-    pickQuestion();
-}
+    //Score tracking 
+    //Code taken from Code Institute Love Math Project
+    function incrementScore() {
+        let oldScore = parseInt(document.getElementById('correct').innerText);
+        document.getElementById('correct').innerText = ++oldScore;
+        pickQuestion();
+    }
 
-function wrongScore() {
-    let oldScore = parseInt(document.getElementById('wrong').innerText);
-    document.getElementById('wrong').innerText = ++oldScore;
-    pickQuestion();
-}
+    function wrongScore() {
+        let oldScore = parseInt(document.getElementById('wrong').innerText);
+        document.getElementById('wrong').innerText = ++oldScore;
+        pickQuestion();
+    }
 
-//When all questions has been answered 
-function endGame() {
-    //Add and remove class hide to only show user result
-    quizDiv.classList.add('hide');
-    questionDiv.classList.add('hide');
-    answerButtons.classList.add('hide');
-    controlButtons.classList.add('hide');
-    resultDiv.classList.remove('hide');
+    //When all questions has been answered 
+    function endGame() {
+        //Add and remove class hide to only show user result
+        quizDiv.classList.add('hide');
+        questionDiv.classList.add('hide');
+        answerButtons.classList.add('hide');
+        controlButtons.classList.add('hide');
+        resultDiv.classList.remove('hide');
 
-    //Get the users correct answered score and show with username
-    let yourResult = document.getElementById('your-score');
+        //Get the users correct answered score and show with username
+        let yourResult = document.getElementById('your-score');
 
-    yourResult.innerText = `is a total of `+ parseInt(document.getElementById('correct').innerText) + ` correct answers`;
+        yourResult.innerText = localStorage.getItem('username') + ` has a total of ` + parseInt(document.getElementById('correct').innerText) + ` correct answers`;
+    }
 
-    console.log('result');
+    //Reset to a new game
+    function reset() {
+        location.reload();
+    }
 
-}
-
-//Reset to a new game
-function reset() {
-    location.reload();
-}
-
-//Starts the game
-function goGame() {
-    console.log('started');
-    answerButtons.classList.remove('hide');
-    startButton.classList.add('hide');
-    nextButton.classList.remove('hide');
-    pickQuestion();
+    //Starts the game
+    function goGame() {
+        answerButtons.classList.remove('hide');
+        startButton.classList.add('hide');
+        nextButton.classList.remove('hide');
+        pickQuestion();
+    }
 }
